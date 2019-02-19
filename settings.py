fd4a27b8c53a96b9f9fc5231f0126064fc405798
Jonatan Heyman <jonatan@heyman.info>
2019-02-19 21:23:46 +0100

    Updated base Github URL

--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
 import os
 import os.path
 
-REMOTE_DATA_BASE_URL = "https://raw.github.com/peterjaric/rayter-games/master/"
+REMOTE_DATA_BASE_URL = "https://raw.githubusercontent.com/peterjaric/rayter-games/master/"
 REDIS_URL = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')

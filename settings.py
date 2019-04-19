9fc7b050185a2f192862a49651320973a6dc5ad0
Peter Jaric <register@jaric.org>
2019-04-19 19:36:39 +0200

    Add missing file. Switch back to correct Github base url.

--- settings.py
+++ settings.py
@@ -2,7 +2,7 @@ import os
 import os.path
 
 REMOTE_DATA_BASE_URL = (
-    "https://raw.githubusercontent.com/rayterbot/rayter-games/master/")
+    "https://raw.githubusercontent.com/peterjaric/rayter-games/master/")
 REDIS_URL = os.getenv("REDISTOGO_URL", "redis://localhost:6379")
 RAYTER_GITHUB_TOKEN = os.getenv("RAYTER_GITHUB_TOKEN")
 RAYTER_USERS = os.getenv("RAYTER_USERS")
